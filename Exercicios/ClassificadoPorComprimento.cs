81b1614aa74c8d0b3f5341ba9f429d3883a10d37
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2023-06-10 14:11:50 -0300

    Iniciando um novo problema

---
+++ Exercicios/ClassificadoPorComprimento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    internal class ClassificadoPorComprimento
+    {
+        /*
+         Crie um programa para ordenar um conjunto de strings por seu tamanho. Seu programa é receber um conjunto de strings e retornar por este mesmo tamanho ordenado de palavras, se o tamanho das strings forem iguais, deve manter a ordem original do conjunto.
+
+        Entrada
+        A primeira linha de entrada possui um inteiro único N que indica o número de conjuntos de strings, cada conjunto pode conter entre 1 e 50 elementos inclusivos e cada uma das strings do conjunto pode conter entre 1 e 50 caracteres inclusivos.
+
+        Saída
+        A saída deve conter o conjunto de strings de entrada ordenados pelo comprimento das strings. Um espaço em branco deve ser impresso entre duas palavras.
+
+        Entrada de amostra                                                    	Saída de amostra
+
+        4                                                                       meia-noite Coder comp Quarta Top às
+        Top Coder comp Quarta à meia-noite                                      três cinco um
+        um três cinco                                                           amor Cpp I
+        Eu amo Cpp                                                              sj sa df sd fd arewfdsavcxz
+        sj a sa df rewfdsavcxz sd fd                                                                                                                                                                                                                 
+         */
+        public static void Main()
+        {
+
+        }
+    }
+}
